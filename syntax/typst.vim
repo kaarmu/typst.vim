kaarmu/typst.vim

commit 7d5c1ee6d4703136975e06c3ac5624e39b28e00f

fix: matching for identifier, references, labels and ratios

syntax/typst.vim
@@ -81,7 +81,7 @@ syntax match typstCodeNumberAngle
     \ /\v<\d+(\.\d*)?(deg|rad)>/
 syntax match typstCodeNumberRatio
     \ contained
-    \ /\v<\d+(\.\d*)?\%>/
+    \ /\v<\d+(\.\d*)?\%/
 syntax match typstCodeNumberFraction
     \ contained
     \ /\v<\d+(\.\d*)?fr>/
@@ -90,16 +90,16 @@ syntax region typstCodeString
     \ start=/"/ skip=/\v\\\\|\\"/ end=/"/
     \ contains=@Spell
 
-" Code > Identifiers {{{2
+" Code > Identifiers- {{{2
 syntax cluster typstCodeIdentifiers
     \ contains=typstCodeIdentifier
             \ ,typstCodeFieldAccess
 syntax match typstCodeIdentifier
     \ contained
-    \ /\v\K\k*%(-+\k+)*>-@!(<%(let|set|show|import|include))@<![\.\[\(]@!/
+    \ /\v\K%(\k*-*)*(<%(let|set|show|import|include))@<![\.\[\(]@!/
 syntax match typstCodeFieldAccess
     \ contained
-    \ /\v\K\k*%(-+\k+)*>-@!(<%(let|set|show|import|include))@<!\.[\[\(]@!/
+    \ /\v\K%(\k*-*)*(<%(let|set|show|import|include))@<!\.[\[\(]@!/
     \ nextgroup=typstCodeFieldAccess,typstCodeFunction
 
 " Code > Functions {{{2
@@ -107,7 +107,7 @@ syntax cluster typstCodeFunctions
     \ contains=typstCodeFunction
 syntax match typstCodeFunction
     \ contained
-    \ /\v\K\k*%(-+\k+)*[\(\[]@=/
+    \ /\v\K%(\k*-*)*[\(\[]@=/
     \ nextgroup=typstCodeFunctionArgument
 syntax match typstCodeFunctionArgument
     \ contained
@@ -176,16 +176,16 @@ syntax cluster typstHashtagIdentifiers
     \ contains=typstHashtagIdentifier
             \ ,typstHashtagFieldAccess
 syntax match typstHashtagIdentifier
-    \ /\v#\K\k*%(-+\k+)*>-@!(<%(let|set|show|import|include))@<![\.\[\(]@!/
+    \ /\v#\K%(\k*-*)*(<%(let|set|show|import|include))@<![\.\[\(]@!/
 syntax match typstHashtagFieldAccess
-    \ /\v#\K\k*%(-+\k+)*>-@!(<%(let|set|show|import|include))@<!\.[\[\(]@!/
+    \ /\v#\K%(\k*-*)*>(<%(let|set|show|import|include))@<!\.[\[\(]@!/
     \ nextgroup=typstCodeFieldAccess,typstCodeFunction
 
 " Hashtag > Functions {{{2
 syntax cluster typstHashtagFunctions
     \ contains=typstHashtagFunction
 syntax match typstHashtagFunction
-    \ /\v#\K\k*%(-+\k+)*[\(\[]@=/
+    \ /\v#\K%(\k*-*)*[\(\[]@=/
     \ nextgroup=typstCodeFunctionArgument
 
 " Hashtag > Parens {{{2
@@ -238,9 +238,9 @@ syntax match typstMarkupRawInline
 syntax region typstMarkupRawBlock
     \ start=/```/ end=/```/
 syntax match typstMarkupLabel
-    \ /<\S\+>/
+    \ /\v\<\K%(\k*-*)*\>/
 syntax match typstMarkupReference
-    \ /@\S\+\s/
+    \ /\v\@\K%(\k*-*)*/
 syntax match typstMarkupHeading
     \ /^=\{1,6}\s.*$/
 syntax match typstMarkupBulletList

commit b51307c84ead11f939a2ed0122dfe58efa5e1afd

fix: parsing bug introduced in 7d5c1ee

syntax/typst.vim
@@ -98,10 +98,10 @@ syntax cluster typstCodeIdentifiers
             \ ,typstCodeFieldAccess
 syntax match typstCodeIdentifier
     \ contained
-    \ /\v\K%(\k*-*)*(<%(let|set|show|import|include))@<![\.\[\(]@!/
+    \ /\v\K\k*%(-+\k+)*>-@!(<%(let|set|show|import|include))@<![\.\[\(]@!/
 syntax match typstCodeFieldAccess
     \ contained
-    \ /\v\K%(\k*-*)*(<%(let|set|show|import|include))@<!\.[\[\(]@!/
+    \ /\v\K\k*%(-+\k+)*>-@!(<%(let|set|show|import|include))@<!\.[\[\(]@!/
     \ nextgroup=typstCodeFieldAccess,typstCodeFunction
 
 " Code > Functions {{{2
@@ -109,7 +109,7 @@ syntax cluster typstCodeFunctions
     \ contains=typstCodeFunction
 syntax match typstCodeFunction
     \ contained
-    \ /\v\K%(\k*-*)*[\(\[]@=/
+    \ /\v\K\k*%(-+\k+)*[\(\[]@=/
     \ nextgroup=typstCodeFunctionArgument
 syntax match typstCodeFunctionArgument
     \ contained
@@ -178,16 +178,16 @@ syntax cluster typstHashtagIdentifiers
     \ contains=typstHashtagIdentifier
             \ ,typstHashtagFieldAccess
 syntax match typstHashtagIdentifier
-    \ /\v#\K%(\k*-*)*(<%(let|set|show|import|include))@<![\.\[\(]@!/
+    \ /\v#\K\k*%(-+\k+)*>-@!(<%(let|set|show|import|include))@<![\.\[\(]@!/
 syntax match typstHashtagFieldAccess
-    \ /\v#\K%(\k*-*)*>(<%(let|set|show|import|include))@<!\.[\[\(]@!/
+    \ /\v#\K\k*%(-+\k+)*>-@!(<%(let|set|show|import|include))@<!\.[\[\(]@!/
     \ nextgroup=typstCodeFieldAccess,typstCodeFunction
 
 " Hashtag > Functions {{{2
 syntax cluster typstHashtagFunctions
     \ contains=typstHashtagFunction
 syntax match typstHashtagFunction
-    \ /\v#\K%(\k*-*)*[\(\[]@=/
+    \ /\v#\K\k*%(-+\k+)*[\(\[]@=/
     \ nextgroup=typstCodeFunctionArgument
 
 " Hashtag > Parens {{{2